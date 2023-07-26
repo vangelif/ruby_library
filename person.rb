122385814edad9570aa299b3698ec352dfb4e3d0
vangelif <ev.fotiades@yahoo.com>
2023-07-26 18:20:27 +0300

    adjust person class to inherit from nameable

--- person.rb
+++ person.rb
@@ -1,7 +1,10 @@
-class Person
+require 'nameable'
+
+class Person < Nameable
   attr_accessor :id, :name, :age
 
   def initialize(name = 'Unknown', age = nil, parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -10,6 +13,10 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    name
   end
 
   private
