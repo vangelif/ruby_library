8c1ed881f530149cdb447a9d2a68ea035a861cf9
Luis Zubia <soyzubia@gmail.com>
2023-07-31 23:26:17 -0700

    refactor main class to use choice module

--- main.rb
+++ main.rb
@@ -1,29 +1,13 @@
 require_relative 'app'
 require_relative 'modules/render'
+require_relative 'modules/choice'
 
 class Main
   include Render
+  include Choice
+
   def initialize
     @app = App.new
-  end
-
-  def choose_a_number(choice)
-    case choice
-    when 1
-      @app.list_books
-    when 2
-      @app.list_people
-    when 3
-      @app.create_a_person
-    when 4
-      @app.create_a_book
-    when 5
-      @app.create_a_rental
-    when 6
-      @app.list_rentals
-    else
-      puts '👋 Sorry to see you leaving..'
-    end
   end
 
   def run
@@ -32,8 +16,10 @@ class Main
       choice = gets.chomp.to_i
       break if choice == 7
 
-      choose_a_number(choice)
+      choose_a_number(@app, choice)
     end
+
+    puts '👋 Sorry to see you leaving..'
   end
 end
 
