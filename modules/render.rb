6bf42305d13304710c4add0b36cce7ade4422d5f
vangelif <ev.fotiades@yahoo.com>
2023-08-01 16:27:11 +0300

    build render choices module

---
+++ modules/render.rb
@@ -0,0 +1,12 @@
+module Render
+  def render_choices
+    puts 'Choose your Option:'
+    puts '1. List all books.'
+    puts '2. List all people.'
+    puts '3. Create a person.'
+    puts '4. Create a book.'
+    puts '5. Create a rental.'
+    puts '6. List all rentals for a given person id.'
+    puts '7. Quit'
+  end
+end
