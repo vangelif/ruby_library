9914c32a52d1aa824dcf15bfb376390ed374774b
vangelif <ev.fotiades@yahoo.com>
2023-08-01 16:27:25 +0300

    refactor main class

--- main.rb
+++ main.rb
@@ -1,44 +1,41 @@
 require_relative 'app'
+require_relative 'modules/render'
 
-def render_choices
-  puts 'Choose your Option:'
-  puts '1. List all books.'
-  puts '2. List all people.'
-  puts '3. Create a person.'
-  puts '4. Create a book.'
-  puts '5. Create a rental.'
-  puts '6. List all rentals for a given person id.'
-  puts '7. Quit'
-end
+class Main
+  include Render
+  def initialize
+    @app = App.new
+  end
 
-def choose_a_number(choice, app)
-  case choice
-  when 1
-    app.list_books
-  when 2
-    app.list_people
-  when 3
-    app.create_a_person
-  when 4
-    app.create_a_book
-  when 5
-    app.create_a_rental
-  when 6
-    app.list_rentals
-  else
-    puts '👋 Sorry to see you leaving..'
+  def choose_a_number(choice)
+    case choice
+    when 1
+      @app.list_books
+    when 2
+      @app.list_people
+    when 3
+      @app.create_a_person
+    when 4
+      @app.create_a_book
+    when 5
+      @app.create_a_rental
+    when 6
+      @app.list_rentals
+    else
+      puts '👋 Sorry to see you leaving..'
+    end
+  end
+
+  def run
+    loop do
+      render_choices
+      choice = gets.chomp.to_i
+      break if choice == 7
+
+      choose_a_number(choice)
+    end
   end
 end
 
-def main
-  app = App.new
-  loop do
-    render_choices
-    choice = gets.chomp.to_i
-    break if choice == 7
-
-    choose_a_number(choice, app)
-  end
-end
-
-main
+main = Main.new
+main.run
