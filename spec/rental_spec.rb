4dccdfeb50544be2515b499f2ff77cc98d351945
Luis Zubia <soyzubia@gmail.com>
2023-08-02 00:06:41 -0700

    Fix rubocop trailing spaces linters

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -10,21 +10,21 @@ describe Rental do
       rental = Rental.new('2023-08-02', book, person)
       expect(rental).to be_an_instance_of Rental
     end
-    
+
     it 'the date attribute for the rental is "2023-08-02"' do
       person = Teacher.new(name: 'Mr. Smith', age: 35, specialization: 'Chemistry')
       book = Book.new(title: 'Chemistry for newbies', author: 'The teacher')
       rental = Rental.new('2023-08-02', book, person)
       expect(rental.date).to eql '2023-08-02'
     end
-    
+
     it 'the book attribute for the rental is an instance of Book' do
       person = Teacher.new(name: 'Mr. Smith', age: 35, specialization: 'Chemistry')
       book = Book.new(title: 'Chemistry for newbies', author: 'The teacher')
       rental = Rental.new('2023-08-02', book, person)
       expect(rental.book).to be_an_instance_of Book
     end
-    
+
     it 'the person attribute for the rental is an instance of Teacher' do
       person = Teacher.new(name: 'Mr. Smith', age: 35, specialization: 'Chemistry')
       book = Book.new(title: 'Chemistry for newbies', author: 'The teacher')
@@ -32,7 +32,7 @@ describe Rental do
       expect(rental.person).to be_an_instance_of Teacher
     end
   end
-  
+
   describe 'to the book rented' do
     it 'the created rental is added' do
       person = Teacher.new(name: 'Mr. Smith', age: 35, specialization: 'Chemistry')
