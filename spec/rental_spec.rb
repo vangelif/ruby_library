9641fed8d78f223450a947c0676f3003245fb6c7
Luis Zubia <soyzubia@gmail.com>
2023-08-02 12:24:58 -0700

    Add 'to_s' and 'to_json' test in Rental class

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -50,4 +50,39 @@ describe Rental do
       expect(person.rentals).to include rental
     end
   end
+
+  describe 'a Rental is displayed correctly' do
+    it '\'to_s\' method returns a custom string to display a rental' do
+      person = Teacher.new(name: 'Mr. Smith', age: 35, specialization: 'Chemistry')
+      book = Book.new(title: 'Chemistry for newbies', author: 'The teacher')
+      rental = Rental.new('2023-08-02', book, person)
+      expect(rental.to_s).to eql('Date: 2023-08-02, 📚 Title: Chemistry for newbies by The teacher 🤝')
+    end
+  end
+
+  describe 'a Rental is converted to JSON for saving it to \'rentals.json\' file' do
+    it '\'to_json\' method returns JSON hash object ready to write it to \'rentals.json\' file' do
+      person = Teacher.new(name: 'Mr. Smith', age: 35, specialization: 'Chemistry', id: 1313)
+      book = Book.new(title: 'Chemistry for newbies', author: 'The teacher', id: 1020)
+      rental = Rental.new('2023-08-02', book, person)
+      p rental.to_json.class
+      expect(rental.to_json).to be_an_instance_of String
+      expect(JSON.parse(rental.to_json)).to be_an_instance_of Hash
+    end
+
+    it '\'to_json\' returned value can be parsed to a Hash' do
+      person = Teacher.new(name: 'Mr. Smith', age: 35, specialization: 'Chemistry', id: 1313)
+      book = Book.new(title: 'Chemistry for newbies', author: 'The teacher', id: 1020)
+      rental = Rental.new('2023-08-02', book, person)
+      expect(JSON.parse(rental.to_json)).to be_an_instance_of Hash
+    end
+
+    it '\'to_json\' method returns a valid Rental JSON string' do
+      person = Teacher.new(name: 'Mr. Smith', age: 35, specialization: 'Chemistry', id: 1313)
+      book = Book.new(title: 'Chemistry for newbies', author: 'The teacher', id: 1020)
+      rental = Rental.new('2023-08-02', book, person)
+      rental_hash = { date: '2023-08-02', book_id: 1020, person_id: 1313 }
+      expect(JSON.parse(rental.to_json)).to eql(JSON.parse(JSON.dump(rental_hash)))
+    end
+  end
 end
