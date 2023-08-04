99ea24d9e111581f08b96514a809231438ad62e8
vangelif <ev.fotiades@yahoo.com>
2023-08-04 13:15:47 +0300

    fix linters

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -61,50 +61,49 @@ describe Book do
     book = Book.new(title: 'title two', author: 'author two', id: 47)
     expect(book.id).to eql 47
   end
-  #additions
+  # additions
   let(:book_title) { 'Test Book' }
   let(:book_author) { 'Test Author' }
   let(:book_id) { 123 }
-    # Helper method to create a new book instance for each test
-    def new_book
-      Book.new(title: book_title, author: book_author, id: book_id)
-    end
+  # Helper method to create a new book instance for each test
+  def new_book
+    Book.new(title: book_title, author: book_author, id: book_id)
+  end
 
-    describe '#add_rental' do
-      it 'adds a new rental to the rentals array' do
-        book = new_book
-        rental_date = Date.today
+  describe '#add_rental' do
+    it 'adds a new rental to the rentals array' do
+      book = new_book
+      rental_date = Date.today
 
-        person = Person.new(name: 'John Doe', age: 25) # Create a Person instance
-        rental = book.add_rental(rental_date, person) # Pass the person instance as the second argument
-  
-        expect(book.rentals).not_to be_empty
-        expect(book.rentals.size).to eq(1)
-        expect(book.rentals.first.date).to eq(rental_date)
-        expect(book.rentals.first.book).to eq(book)
-        expect(book.rentals.first.person).to eq(person)
-      end
-    end
+      person = Person.new(name: 'John Doe', age: 25) # Create a Person instance
+      book.add_rental(rental_date, person) # Pass the person instance as the second argument
 
-    describe '#to_json' do
-      it 'returns a valid JSON representation of the book' do
-        book = new_book
-        expected_json = {
-          id: book_id,
-          title: book_title,
-          author: book_author
-        }.to_json
-  
-        expect(book.to_json).to eq(expected_json)
-      end
+      expect(book.rentals).not_to be_empty
+      expect(book.rentals.size).to eq(1)
+      expect(book.rentals.first.date).to eq(rental_date)
+      expect(book.rentals.first.book).to eq(book)
+      expect(book.rentals.first.person).to eq(person)
     end
-    describe '#to_s' do
-      it 'returns a formatted string representation of the book' do
-        book = new_book
-        expected_string = "📚 Title: #{book_title} by #{book_author}"
-  
-        expect(book.to_s).to eq(expected_string)
-      end
+  end
+
+  describe '#to_json' do
+    it 'returns a valid JSON representation of the book' do
+      book = new_book
+      expected_json = {
+        id: book_id,
+        title: book_title,
+        author: book_author
+      }.to_json
+
+      expect(book.to_json).to eq(expected_json)
     end
-  
+  end
+  describe '#to_s' do
+    it 'returns a formatted string representation of the book' do
+      book = new_book
+      expected_string = "📚 Title: #{book_title} by #{book_author}"
+
+      expect(book.to_s).to eq(expected_string)
+    end
+  end
 end
