286b5fd17515a2fe22f42cc5cc06c2dc8c7ba6ce
Vangelis <ev.fotiades@yahoo.com>
2023-11-14 16:14:57 +0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,15 +2,23 @@ require_relative 'app'
 require_relative 'modules/render'
 require_relative 'modules/choice'
 
+# STEP SEVEN: build Main class and include Render and Choice modules    
 class Main
+
+  # STEP SIX: build Render and Choice modules
+  # check on the module folder 
   include Render
   include Choice
 
+  # STEP EIGHT: build an instantation from the App class
   def initialize(input_source: $stdin)
     @app = App.new
     @input_source = input_source
   end
 
+  # STEP NINE: build a run method ie. a loop to 
+  # A. display choices and
+  # B. handle input from the user
   def run
     loop do
       render_choices
